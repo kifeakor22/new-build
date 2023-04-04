5c5f2d52d52f4ac113801dcdd23fe7f31facd6bb
Kc <kifeakor@yahoo.com>
2023-04-04 11:43:11 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Helmet} from "react-helmet";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import './App.css';
+import NavBar from './components/navbar';
+import Herogrid from './components/herogrid';
+import Projects from './components/projects';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>New-Build</title>
+      </Helmet>
+      <NavBar/>
+      <Herogrid/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/herogrid/index.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import './style.css'
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+
+
+const Herogrid = () => {
+  return (
+    <>
+    <Box sx={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+      <Grid container spacing={1} sx={{ maxWidth: '10000px', width: '100%', position: 'relative' }}>
+        <Grid item xs={12} sm={4} md={4}>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <img
+              className="profilePic"
+              src={require("./images/img.png")}
+              width="200"
+              height="auto"
+              alt="profile"
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={8} md={8}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'left' }}>
+            <Typography variant="h1">
+              Hello my name is KC
+            </Typography>
+            <Typography variant="h2">
+              A UX/UI Frontend developer
+            </Typography>
+            <Typography variant="h5">
+              With great focus on accessibility and mobile-first responsive designs. Web development allows me to combine my creative and technical skills in a way that is both challenging and rewarding.
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      
+    </Box>
+    <div>
+        <Divider className='Divider' textAlign="right">
+            <Button variant="contained" size='large' style={{backgroundColor: 'grey'}}>Contact Me</Button>
+        </Divider>
+
+    </div>
+    </>
+    
+  );
+};
+
+export default Herogrid;
