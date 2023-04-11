a47e7cb18a4ebdeb60945ed1366bd8d5fad758ff
Kc <kifeakor@yahoo.com>
2023-04-11 14:39:05 +0100

    added links to footer icons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,20 +5,27 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import {Box} from '@mui/material';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import {Typography} from '@mui/material';
+import './style.css'
 
 
 const Footer = () => {
     return(
         <>
          <Divider sx={{color: 'black'}} className='Divider' textAlign='left'>
-            <Typography variant= 'h3' className='navicon' >Follow me</Typography>
+            <Typography variant= 'h5' className='navicon' >Follow me</Typography>
         </Divider>
        
         <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap', gap: '20px', padding: '20px' }}>
-            
-        <GitHubIcon sx={{}}  fontSize='large'/>
-        <LinkedInIcon  fontSize='large'/>
-        <TwitterIcon  fontSize='large'/>
+         <a href="https://github.com/kifeakor22" target="_blank">   
+        <GitHubIcon sx={{color: 'black'}}  fontSize='large'/>
+        </a>
+          <a href="https://https://www.linkedin.com/in/kc-ifeakor-b79a6031/" target="_blank">
+            <LinkedInIcon sx={{color: 'black'}} className='icon' fontSize='large'/>
+        </a>
+         <a href="https://twitter.com/?lang=en" target="_blank">
+        <TwitterIcon sx={{color: 'black'}} fontSize='large'/>
+        </a>
+        <p>&copy; KC Ifeakor 2022. All Rights Reserved.</p>
         </Box>
         </>
     )
